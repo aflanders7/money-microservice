7050256fa2a08b242e008288b5630bc075b24d4f
Audrey Flanders <114197067+aflanders7@users.noreply.github.com>
2023-11-14 20:24:42 -0500

    Request microservice

---
+++ request_microservice.py
@@ -0,0 +1,29 @@
+import time
+
+
+def validate_input(input):
+    """Receives a user-given input. Returns True if input is a valid integer or decimal number."""
+    try:
+        float(input)
+        if str(input)[::-1].find('.') <= 2:
+            return True
+        else:
+            print("Error: Only 2 decimals places allowed")
+            return False
+    except ValueError:
+        print("Error: Please enter an integer or decimal")
+        return False
+
+
+def call_microservice(value):
+    """Takes a monetary value as a parameter. Calls the microservice and receives data via money-service.txt"""
+    if validate_input(value) is True:
+        print("valid input")
+        txt_file = open("money-service.txt", 'w')
+        txt_file.write(value)
+        txt_file.close()
+        time.sleep(5)
+        txt_file = open("money-service.txt", 'r')
+        content = txt_file.readline()
+        print(content)
+        return content
